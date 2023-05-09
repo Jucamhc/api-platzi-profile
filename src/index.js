b63a79a71bbb742ae127b0f211bb724fd2ac4082
Kamilo2251 <camilo2251@gmail.com>
2023-05-09 13:58:47 -0500

    a

--- src/index.js
+++ src/index.js
@@ -110,7 +110,8 @@ app.get('/api_profile/:id', async (req, res) => {
                 jsonData_username_profile_url = JSON.parse("{" + jsonData_username_profile_url[0] + "}");
                 jsonData_username_profile_url.courses = jsonCourses
                 res.setHeader('Content-Type', 'application/json');
-                res.setHeader('Access-Control-Allow-Origin', '*');
+                res.header("Access-Control-Allow-Origin", "*");
+                res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
                 res.send(JSON.stringify(jsonData_username_profile_url));
             }
 
